61301bd01847bb6ee8f8d98417283141a33eca8a
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-03-19 18:35:28 +0000

    feat: add passguard showcase

---
+++ projects/passguard/showcase/index.js
@@ -0,0 +1,30 @@
+let array = ["img/1.png", "img/2.png", "img/3.png", "img/4.png", "img/5.png"]
+let currentImage = 0;
+
+function nextImage() {
+    currentImage = currentImage + 1;
+
+    if (currentImage > array.length - 1){
+        currentImage = 0;
+    }
+
+    document.getElementById("imgElement").src = array[currentImage];
+
+    updateText();
+}
+
+function previousImage() {
+    currentImage = currentImage - 1;
+
+    if (currentImage < 0) {
+        currentImage = array.length - 1;
+    }
+
+    document.getElementById("imgElement").src = array[currentImage];
+
+    updateText();
+}
+
+function updateText() {
+    document.getElementById("bottom").innerHTML = (currentImage + 1) + "/" + array.length;
+}
